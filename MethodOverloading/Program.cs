f20f28ef92b2bdf8e00b57a8a5ba92ddaf762a5f
Alexander Holder <drowish95z@gmail.com>
2022-12-15 08:59:31 -0600

    Simple Overloading Exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Example functions are below
+
+            //Tested Functions Below
+            /*Console.WriteLine(Add(1, 7));
+            Console.WriteLine(Add(1.5, 2.75));
+            Console.WriteLine(Add(1, 2, true));
+            Console.WriteLine(Add(1, 2, false));*/
+        }
+        static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        static string Add(int num1, int num2, bool param)
+        {
+            string result = "";
+            int sum = num1 + num2;
+            if (param == true)
+            {
+                result = $"{sum} dollar";
+                if (sum != 1 && sum != -1) result += "s";
+            }
+            return result;
         }
     }
 }
